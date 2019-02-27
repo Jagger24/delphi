61be4de94626b1313097637500435d3d6e7e7857
dannymart123 <dannymart123>
2019-02-26 21:40:16 -0500

    More Test Cases

---
+++ delphi/tests/Feature/welcomePageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class welcomePageTest extends TestCase
+{
+    // /**
+    //  * Test to make sure "About delphi" link takes you to about page.
+    //  *
+    //  * @return void
+    //  */
+    // public function testAboutDelphiLink()
+    // {
+    //     $this->visit('/')->click('About Delphi')->seePageIs('/about');
+    // }
+
+}
