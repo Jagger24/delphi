95d079e2060c858fed40a3cff81b768b9e46d7eb
Brandon <brown.5562@osu.edu>
2019-04-09 12:14:40 -0400

    Added stats link to stats cards and remove link to stats from home page

--- delphi/resources/views/stats.blade.php
+++ delphi/resources/views/stats.blade.php
@@ -12,16 +12,15 @@
 				<div class="card" style="width: 18rem;">
 					<div class="card-body">
 						<h5 class="card-title"> Delphi Code: {{ $sessioned['code'] }}</h5>
-						<h5 class="card-text"> Method: </h5>
-						@if(array_key_exists("groups", $sessioned))
-							@foreach($sessioned['groups'] as $group)
-								<h5 class="card-text"> Activity: @if($group['active'] == 0) Not Active @else Active @endif </h5>
-								<h5 class="card-text"> Std. Deviation: </h5>
-							@endforeach
-							<a href=""> <button class="btn btn-primary" value="Delphi Code Stats"> Statistics </button> </a>
-						@else
-							<a href=""> <button class="btn btn-secondary" value="Delphi Code Stats" disabled> Statistics </button> </a>
-						@endif
+						@if (array_key_exists("groups", $sessioned))
+                        	@foreach($sessioned['groups'] as $group)
+                        		<h5 class="card-text"> Activity: @if($group['active'] == 0) Not Active @else Active @endif </h5>
+                        	@endforeach
+                        	<a href="group/{{ $sessioned['code'] }}/{{$group['id']}}"> <button class="btn btn-primary" value="Delphi Code Stats"> Statistics </button> </a>
+                        @else
+                       		<h5 class="card-text"> Activity: Not Active </h5>
+                       		<a href=""> <button class="btn btn-secondary" value="Delphi Code Stats" disabled> Statistics </button> </a>
+                    	@endif
 					</div>
 				</div>
 			@endforeach
